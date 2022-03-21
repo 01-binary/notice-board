1c581ef71e132c65d8406f3b67247d44e94f2273
jinsoo <binary01@kakao.com>
2022-03-21 16:42:24 +0900

    feat: rtk, saga add

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import { GlobalStyle } from '@src/styles';
+import { store } from '@src/store';
+
 import App from '@src/App';
 
 ReactDOM.render(
